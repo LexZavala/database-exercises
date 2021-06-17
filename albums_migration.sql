2f28fc32db6ab03e83e6284547df90329e62823c
Lex Zavala <lex.zavala7@gmail.com>
2021-06-17 15:31:13 -0500

    created a script for SQL

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,2 +1,14 @@
 USE codeup_test_db;
 
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR (50),
+    record_name VARCHAR (50),
+    release_date DATE (YYYY-MM-DD),
+    sales FLOAT,
+    genre VARCHAR (20),
+    PRIMARY KEY (id)
+);
+
