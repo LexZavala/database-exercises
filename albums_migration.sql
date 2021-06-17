16cf77b048fc42363add08d7421d248ae68be992
Lex Zavala <lex.zavala7@gmail.com>
2021-06-17 15:51:16 -0500

    refactored to avoid error on syntax

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,11 +2,11 @@ USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
-CREATE TABLE IF NOT EXISTS albums (
+CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR (50),
     record_name VARCHAR (50),
-    release_date DATE (YYYY-MM-DD),
+    release_date INT UNSIGNED NOT NULL,
     sales FLOAT,
     genre VARCHAR (20),
     PRIMARY KEY (id)
