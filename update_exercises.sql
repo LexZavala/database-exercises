ddf64cfc4aea1be48431886be84fb66365850372
Lex Zavala <lex.zavala7@gmail.com>
2021-06-18 12:30:57 -0500

    working on updating the values

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,3 +1,9 @@
-# USE codeup_test_db;
-#
-# SELECT * FROM albums;
+USE codeup_test_db;
+
+SELECT record_name AS 'All albums on table' FROM albums;
+SELECT record_name AS 'Albums released before 1980' FROM albums WHERE release_date < 1980;
+SELECT record_name AS 'Albums by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
+
+UPDATE albums
+SET
+WHERE
