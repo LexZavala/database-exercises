da84b1b79b4efcf013e0876209a94416b453f45a
Lex Zavala <lex.zavala7@gmail.com>
2021-06-18 14:07:48 -0500

    completed the update exercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,9 +1,21 @@
 USE codeup_test_db;
 
 SELECT record_name AS 'All albums on table' FROM albums;
-SELECT record_name AS 'Albums released before 1980' FROM albums WHERE release_date < 1980;
-SELECT record_name AS 'Albums by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
-
 UPDATE albums
-SET
-WHERE
+SET sales = sales * 10;
+SELECT *  FROM albums;
+
+SELECT record_name AS 'Albums released before 1980' FROM albums WHERE release_date < 1980;
+UPDATE albums
+SET release_date = '1800'
+WHERE release_date <1980;
+SELECT * FROM albums;
+
+SELECT record_name AS 'Albums by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+SELECT * FROM albums;
+
+
+
