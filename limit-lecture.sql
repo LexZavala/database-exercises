22fda33c7e939d3b3ce601d913b93d7d7ad2d557
Lex Zavala <lex.zavala7@gmail.com>
2021-06-21 14:08:37 -0500

    added comment

--- limit-lecture.sql
+++ limit-lecture.sql
@@ -7,4 +7,6 @@ LIMIT 20;
 SELECT * FROM employees WHERE birth_date LIKE '1954-01%'
 ORDER BY birth_date, last_name
 LIMIT 20 OFFSET 20;
+# Offset allows for you to run a query at a different row index
+
 
