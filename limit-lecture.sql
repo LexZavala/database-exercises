7a3a746672dd1f61bf6fe4620398ee32dcb1bc78
Lex Zavala <lex.zavala7@gmail.com>
2021-06-21 15:23:47 -0500

    created file for lecture on functions

--- limit-lecture.sql
+++ limit-lecture.sql
@@ -9,4 +9,5 @@ ORDER BY birth_date, last_name
 LIMIT 20 OFFSET 20;
 # Offset allows for you to run a query at a different row index
 
-
+#underscore wild card represents one space, % wild card represents any number of spaces
+# '__Sed%'
