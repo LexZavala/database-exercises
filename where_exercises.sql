c8c621105f13e2d0673ba0026e3397c6848deb5a
Lex Zavala <lex.zavala7@gmail.com>
2021-06-21 11:48:42 -0500

    completed exercise for where

--- where_exercises.sql
+++ where_exercises.sql
@@ -14,3 +14,6 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
+
