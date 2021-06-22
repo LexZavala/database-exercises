54eca4052f0fdda0a62690df121e64da2a869f3a
Lex Zavala <lex.zavala7@gmail.com>
2021-06-22 16:04:33 -0500

    lecture

--- joins-lecture.sql
+++ joins-lecture.sql
@@ -16,3 +16,25 @@ LIMIT 50;
 USE join_test_db;
 
 #LEFT JOIN
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users JOIN roles
+ON users.role_id = roles.id;
+# with the above, user names with a null role_id do not show up in the output
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users LEFT JOIN roles
+ON users.role_id = roles.id;
+
+USE codeup_test_db;
+# CREATE TABLE persons (
+#      person_id INT NOT NULL AUTO_INCREMENT,
+#      first_name VARCHAR(25) NOT NULL,
+#      album_id INT NOT NULL,
+#      PRIMARY KEY (person_id)
+# ); already did this
+
+# INSERT INTO persons (first_name, album_id) VALUES ('Olivia', 29), ('Santiago', 27), ('Tareq', 15), ('Anaya', 28);
+# already did this
+
+SELECT p.first_name, a.name FROM persons p JOIN albums a ON p.album_id = a.id;
