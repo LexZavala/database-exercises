dfcedafb5521fd8c6df773fbbf5d7c7f2282ed4e
Lex Zavala <lex.zavala7@gmail.com>
2021-06-22 15:11:28 -0500

    refactored based on walkthrough

--- relationship-index-exercises.sql
+++ relationship-index-exercises.sql
@@ -15,3 +15,4 @@ ADD UNIQUE (artist, record_name);
 INSERT INTO albums (artist, record_name, release_date, sales, genre)
 VALUES ('Michael Jackson', 'Thriller', 2021, 70.3, 'Country');
 
+SHOW INDEXES FROM albums;
