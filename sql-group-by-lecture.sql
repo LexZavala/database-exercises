0cf084ac2f73762c6b051492b7d5b96bff7db947
Lex Zavala <lex.zavala7@gmail.com>
2021-06-22 10:32:07 -0500

    followed the lecture

--- sql-group-by-lecture.sql
+++ sql-group-by-lecture.sql
@@ -1,3 +1,31 @@
 USE employees;
 
-SELECT COUNT(*) FROM employees
+SELECT COUNT(*), gender FROM employees GROUP BY gender;
+
+SELECT COUNT(*), last_name FROM employees GROUP BY last_name ORDER BY count(*);
+
+# SELECT *, gender FROM employees GROUP BY gender;
+# this wont work due to lack of COUNT function to aggregate the data
+
+# Lets find the average age of hire for employees in the company
+SELECT AVG(DATEDIFF(hire_date, birth_date)/365) FROM employees;
+
+SELECT MAX(DATEDIFF(hire_date, birth_date)/365) FROM employees;
+
+SELECT MIN(DATEDIFF(hire_date, birth_date)/365) FROM employees;
+
+# GROUP BY combines duplicates into one single value for each group
+# GROUP BY consolidates rows based on a common column
+
+SELECT DISTINCT title FROM titles;
+
+SELECT DISTINCT last_name FROM employees
+WHERE last_name Like 'E%e'
+GROUP BY last_name
+ORDER BY last_name;
+
+SELECT COUNT(last_name), last_name  FROM employees
+WHERE last_name Like 'E%e'
+GROUP BY last_name
+ORDER BY last_name;
+
