a60975f5bcdb5bbfa372133e4de0e84dcd26c6a3
Lex Zavala <lex.zavala7@gmail.com>
2021-06-22 10:49:19 -0500

    added a file for exercises and completed the first 5 exercises

---
+++ group_by_exercises.sql
@@ -0,0 +1,23 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT DISTINCT last_name FROM employees
+WHERE last_name Like 'E%e'
+GROUP BY last_name;
+
+SELECT DISTINCT first_name, last_name FROM employees
+WHERE last_name Like 'E%e'
+ORDER BY last_name;
+
+SELECT DISTINCT last_name FROM employees
+WHERE last_name LIKE '%q%'
+AND NOT last_name LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT COUNT(last_name) FROM employees
+WHERE last_name LIKE '%q%'
+AND NOT last_name LIKE '%qu%'
+GROUP BY last_name;
+
+
