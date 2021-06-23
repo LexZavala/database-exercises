2504194208cc602f52635228a961ecb4a01cdcc7
Lex Zavala <lex.zavala7@gmail.com>
2021-06-23 13:31:09 -0500

    walkthrough

--- join_exercises.sql
+++ join_exercises.sql
@@ -26,4 +26,51 @@ FROM employees as e
 WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
 
 
-SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+SELECT d.dept_name AS 'Department Name',
+CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM departments d
+LEFT JOIN dept_manager dm
+ON d.dept_no = dm.dept_no
+JOIN employees e on e.emp_no = dm.emp_no
+WHERE dm.to_date = '9999-01-01'
+ORDER BY d.dept_name;
+
+
+# Find the name of all departments currently managed by women.
+SELECT d.dept_name AS 'Department Name',
+CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM departments d
+LEFT JOIN dept_manager dm
+ON d.dept_no = dm.dept_no
+JOIN employees e ON e.emp_no = dm.emp_no
+WHERE (dm.to_date = '9999-01-01' AND e.gender = 'F')
+ORDER BY d.dept_name;
+
+# Find the current titles of employees currently working in the Customer Service department.
+SELECT titles.title AS title, COUNT(*) AS Total
+FROM titles
+JOIN dept_emp de ON titles.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE d.dept_name = 'Customer Service' AND titles.to_date = '9999-01-01'
+GROUP BY title;
+
+#Find the current salary of all current managers.
+SELECT d.dept_name AS 'Department Name',
+CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager',
+s.salary AS Salary
+FROM departments d
+LEFT JOIN dept_manager dm
+ON d.dept_no = dm.dept_no
+JOIN employees e ON e.emp_no = dm.emp_no
+JOIN salaries s ON e.emp_no = s.emp_no
+WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
+ORDER BY d.dept_name;
+
+# Bonus Find the names of all current employees, their department name, and their current manager's name .
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee,
+d.dept_name AS Department,
+CONCAT(e2.first_name, ' ', e2.last_name) AS Manager
+FROM employees e
+LEFT JOIN departments d on e.emp_no = d.emp_no
+
+
