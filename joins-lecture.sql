4229451b28e1dd69891105ed1ba295a000fa9df9
Lex Zavala <lex.zavala7@gmail.com>
2021-06-23 09:50:12 -0500

    completed lecture on joins

--- joins-lecture.sql
+++ joins-lecture.sql
@@ -34,7 +34,24 @@ USE codeup_test_db;
 #      PRIMARY KEY (person_id)
 # ); already did this
 
-# INSERT INTO persons (first_name, album_id) VALUES ('Olivia', 29), ('Santiago', 27), ('Tareq', 15), ('Anaya', 28);
+# INSERT INTO persons (first_name, album_id) VALUES ('Olivia', 2), ('Santiago', 8), ('Tareq', 10), ('Anaya', 16);
 # already did this
 
-SELECT p.first_name, a.name FROM persons p JOIN albums a ON p.album_id = a.id;
+# regular JOIN aka INNER JOIN
+SELECT p.first_name, a.record_name FROM persons p JOIN albums a ON p.album_id = a.id;
+# left JOIN also called OUTER JOINS
+SELECT p.first_name, a.record_name FROM albums a LEFT JOIN persons p ON a.id = p.album_id;
+# right JOIN also called OUTER JOINS
+SELECT p.first_name, a.record_name FROM persons p RIGHT JOIN albums a ON a.id = p.album_id;
+
+# junction tables // associative tables  // join tables
+
+# CREATE TABLE preferences (
+#                              person_id INT NOT NULL,
+#                              album_id INT NOT NULL
+# ); already did this
+
+INSERT INTO preferences (person_id, album_id) VALUES (1, 13), (1, 4), (1, 10), (1, 16), (2, 1), (2, 11), (2, 3), (3, 11), (3, 16), (3, 8);
+
+
+SELECT p.first_name AS name, a.record_name AS album FROM persons p JOIN preferences pf ON p.person_id = pf.person_id JOIN albums a ON pf.album_id = a.id;
